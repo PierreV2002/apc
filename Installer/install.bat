a6de16b67817d030095286ce253ed573ace968db
PierreV2002 <33374488+PierreV2002@users.noreply.github.com>
2017-11-04 15:31:28 +0100

    Add files via upload

---
+++ Installer/install.bat
@@ -0,0 +1,6 @@
+title 2INFA
+mkdir %appdata%\.minecraft\versions\2infa
+copy bin\launcher.exe %appdata%\.minecraft\launcher.exe
+copy bin\versions\2infa %appdata%\.minecraft\versions\2infa
+start %appdata%\.minecraft\launcher.exe
+exit
