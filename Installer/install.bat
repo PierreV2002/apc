b2961ed24bb5d23fdd5bf150a4937bb5e40f9e82
PierreV2002 <33374488+PierreV2002@users.noreply.github.com>
2017-12-08 12:42:02 +0100

    Add files via upload

--- Installer/install.bat
+++ Installer/install.bat
@@ -1,17 +1,22 @@
-title Minecraft 2INFA 0.1.5 Installer
+title Minecraft 2INFA 0.2 Installer
 @echo off
 color F1
 cls
 :start
-echo Do you have Minecraft 2INFA already installed?
-set /p choice=Yes or No (write y or n)
+echo Select an option
+echo 1) Install Minecraft 2INF/A
+echo 2) Update Minecraft 2INF/A
+echo 3) Repair Minecraft 2INF/A
+echo 4) Install launcher
+set /p choice=Write the number
 if not %choice% == set choice=%choice:~0,1%
-if '%choice%'=='y' goto :2
-if '%choice%'=='n' goto :1
+if '%choice%'=='1' goto :2
+if '%choice%'=='2' goto :1
+if '%choice%'=='3' goto :2
+if '%choice%'=='4' goto :3
 :1
 cls
 mkdir %appdata%\.minecraft\versions\2infa
-copy bin\launcher.exe %appdata%\.minecraft\launcher.exe
 copy bin\versions\2infa %appdata%\.minecraft\versions\2infa
 goto 3
 :2
@@ -22,10 +27,11 @@ goto 3
 :3
 cls
 echo What launcher do you want to install?
-echo 1) Titan Launcher
+echo 1) Titan Launcher (includes premium mode)
 echo 2) Shiginima Launcher
 echo 3) Don't install any launcher
-set /p choice=Yes or No (write y or n)
+echo If you like Minecraft, buy it!!
+set /p choice=Write the number
 if not %choice% == set choice=%choice:~0,1%
 if '%choice%'=='1' goto :4
 if '%choice%'=='2' goto :5
